f83835ff36bcf343db84d4f050c4c4aabc5bbe01
Cecilia Zhang <yzh223@u.rochester.edu>
2020-02-19 19:20:20 -0500

    dev.1

--- pawliki/src/lib.rs
+++ pawliki/src/lib.rs
@@ -1,4 +1,3 @@
-use eliza::::alphabet::Alphabet;;
 use eliza::script::{Keyword, Reflection, Script, Synonym, Transform};
 
 use regex::{Captures, Regex};
@@ -8,31 +7,76 @@ use std::error::Error;
 
 #[derive(Default)]
 pub struct Pawliki {
-	script: Script,
+    script: Script,
+    memory: VecDeque<String>,
+    rule_usage: HashMap<String, usize>,
 }
 
 impl Pawliki {
-	// add code here
-    // pub fn respond(&mut self, input: &str) -> String {
-    //     //Convert the input to lowercase and transform words before populating the keystack
-    //     let mut response: Option<String> = None;
-    //     let phrases = get_phrases(&transform(&input.to_lowercase(), &self.script.transforms));
-    //     let (active_phrase, mut keystack) = populate_keystack(phrases, &self.script.keywords);
 
-    //     if let Some(phrase) = active_phrase {
-    //         response = self.get_response(&phrase, &mut keystack);
-    //     }
+	//Initialize Pawliki to reads his script
+    pub fn from_file(location: &str) -> Result<Pawliki, Box<dyn Error>> {
+        let e = Pawliki {
+            script: {
+                Script::from_file(location)?
+            },
+            memory: VecDeque::new(),
+            rule_usage: HashMap::new(),
+        };
 
-    //     if let Some(res) = response {
-    //         res
-    //     } else if let Some(mem) = self.memory.pop_front() {
-    //         //Attempt to use something in memory, otherwise use fallback trick
-    //         info!("Using memory");
-    //         mem
-    //     } else {
-    //         info!("Using fallback statement");
-    //         self.fallback()
-    //     }
-    // }
+        Ok(e)
+    }
+
+
+    pub fn greet(&self) -> String {
+    	match self.script.rand_greet() {
+    		Some(greet) => greet.to_string(),
+    		None => {
+    			String::from("Hello, I am Pawliki")
+    		}
+    	}
+    }
+
+    pub fn farewell(&self) -> String {
+        match self.script.rand_farewell() {
+            Some(farwell) => farwell.to_string(),
+            None => {
+                String::from("Goodbye.") //If farewells are empty, have default
+            }
+        }
+    }
+
+    pub fn fallback(&self) -> String {
+        match self.script.rand_fallback() {
+            Some(fallback) => fallback.to_string(),
+            None => {
+                String::from("Go on.") //A fallback for the fallback - har har
+            }
+        }
+    }
+
+    pub fn respond(&mut self, input: &str) -> String {
+        //Convert the input to lowercase and transform words before populating the keystack
+        // let mut response: Option<String> = None;
+        // let phrases = get_phrases(&transform(&input.to_lowercase(), &self.script.transforms));
+        // let (active_phrase, mut keystack) = populate_keystack(phrases, &self.script.keywords);
+
+        // if let Some(phrase) = active_phrase {
+        //     response = self.get_response(&phrase, &mut keystack);
+        // }
+
+        // if let Some(res) = response {
+        //     res
+        // } else if let Some(mem) = self.memory.pop_front() {
+        //     //Attempt to use something in memory, otherwise use fallback trick
+        //     info!("Using memory");
+        //     mem
+        // } else {
+        //     info!("Using fallback statement");
+        //     self.fallback()
+        // }
+        String::from("Goodbye.")
+    }
+
 }
 
--- pawliki/src/main.rs
+++ pawliki/src/main.rs
@@ -4,8 +4,9 @@
 extern crate log;
 use env_logger;
 
-use pawliki;
+use pawliki::Pawliki;
 use eliza::Eliza;
+
 use std::io::Write;
 use std::{env, io, thread, time};
 
@@ -14,33 +15,35 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        error!("Usage of eliza is: ./eliza [SCRIPT]");
+        error!("Usage of pawliki is: ./pawliki [SCRIPT]");
         panic!("Not enough arguments");
     }
 
-    let mut eliza = Eliza::from_file(&args[1]).expect("Eliza failed to load");
+    let mut pawliki = Pawliki::from_file(&args[1]).expect("Pawliki failed to load");
     println!("\nEnter '/quit' to leave the session.\n");
-    println!("{}\n", eliza.greet()); //eliza greets the user
+    println!("{}\n", pawliki.greet()); //eliza greets the user
 
-    loop {
-        print!("> ");
-        io::stdout().flush().expect("Failed to read line.");
 
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line.");
 
-        match input.as_ref() {
-            "/quit\n" => break,
-            //Based on the rules in the script, eliza responds to the given input
-            _ => {
-                //Insert short delay to make eliza seem like she's thinking
-                thread::sleep(time::Duration::from_millis(300));
-                println!("{}\n", eliza.respond(&input));
-            }
-        }
-    }
+    // loop {
+    //     print!("> ");
+    //     io::stdout().flush().expect("Failed to read line.");
 
-    println!("\n{}", eliza.farewell()); //eliza farewells the user
+    //     let mut input = String::new();
+    //     io::stdin()
+    //         .read_line(&mut input)
+    //         .expect("Failed to read line.");
+
+    //     match input.as_ref() {
+    //         "/quit\n" => break,
+    //         //Based on the rules in the script, eliza responds to the given input
+    //         _ => {
+    //             //Insert short delay to make eliza seem like she's thinking
+    //             thread::sleep(time::Duration::from_millis(300));
+    //             println!("{}\n", eliza.respond(&input));
+    //         }
+    //     }
+    // }
+
+    // println!("\n{}", eliza.farewell()); //eliza farewells the user
 }
